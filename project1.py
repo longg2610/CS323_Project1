7eb3abd573d2616d8cd97e345e0c91e24dd56dc9
ithannah <ithannah07@gmail.com>
2025-09-10 15:37:09 -0400

    generalize FCVC

--- project1.py
+++ project1.py
@@ -10,3 +10,6 @@ print(df_qi.dtypes)
 print(df_qi["FCVC"].describe())
 print(df_qi["CAEC"].value_counts(dropna=False))
 print(df_qi["MTRANS"].value_counts(dropna=False))
+
+df_qi["FCVC_gen"] = df_qi["FCVC"].map(lambda x: "Low" if x <= 1.3 else "High")
+print(df_qi["FCVC_gen"].head())
