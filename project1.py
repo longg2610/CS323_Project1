ff9773e379198fb250a84616eb1c3f5f234679c8
ithannah <ithannah07@gmail.com>
2025-09-10 15:24:37 -0400

    loading the data

---
+++ project1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+filename = "ObesityDataSet_raw_and_data_sinthetic.csv"
+df = pd.read_csv(filename)
+qi_cols = ["FCVC", "CAEC", "MTRANS"]
+df_qi = df[qi_cols].copy()
+
+print(df_qi.dtypes)
+print(df_qi["FCVC"].describe())
+print(df_qi["CAEC"].value_counts(dropna=False))
+print(df_qi["MTRANS"].value_counts(dropna=False))
