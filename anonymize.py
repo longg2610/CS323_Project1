ce4318a461ddb077aa16b3305edb92368a9f0239
longg2610 <phamduylong26102004@gmail.com>
2025-09-11 23:15:17 -0400

    fixed deep copy bug

--- anonymize.py
+++ anonymize.py
@@ -22,10 +22,6 @@ print("Height max:", df["Height"].max())
 print("Weight min:", df["Weight"].min())
 print("Weight max:", df["Weight"].max())
 
-# df = df.drop("FAVC", axis = 1)
-# df = df.drop("FCVC", axis = 1)
-# df = df.drop("NCP", axis = 1)
-# df = df.drop("CAEC", axis = 1)
 
 def make_buckets(col_min, col_max, step, use_numpy=False):
     # Round min and max to align with step
@@ -62,15 +58,60 @@ print(age_buckets_3)
 
 height_min, height_max = df["Height"].min(), df["Height"].max()
 height_buckets_0 = np.round(make_buckets(height_min, height_max, 0.1, use_numpy=True), 2)
-height_buckets_1 = np.round(make_buckets(height_min, height_max, 0.2, use_numpy=True), 2)
-height_buckets_2 = np.round(make_buckets(height_min, height_max, 0.3, use_numpy=True), 2)
-height_buckets_3 = np.round(make_buckets(height_min, height_max, 0.5, use_numpy=True), 2)
+height_buckets_1 = np.round(make_buckets(height_min, height_max, 0.3, use_numpy=True), 2)
+height_buckets_2 = np.round(make_buckets(height_min, height_max, 0.5, use_numpy=True), 2)
+height_buckets_3 = np.round(make_buckets(height_min, height_max, 0.7, use_numpy=True), 2)
+
+print(height_buckets_0)
+print(height_buckets_1)
+print(height_buckets_2)
+print(height_buckets_3)
 
 weight_min, weight_max = df["Weight"].min(), df["Weight"].max()
 weight_buckets_0 = make_buckets(weight_min, weight_max, 5)
 weight_buckets_1 = make_buckets(weight_min, weight_max, 20)
 weight_buckets_2 = make_buckets(weight_min, weight_max, 50)
 weight_buckets_3 = make_buckets(weight_min, weight_max, 100)
+
+print(weight_buckets_0)
+print(weight_buckets_1)
+print(weight_buckets_2)
+print(weight_buckets_3)
+
+
+def age_generalize(age, age_buckets):
+    for bucket in age_buckets:
+        if age >= bucket:
+            return bucket
+    # print("last bucket: ", age_buckets[-1], " age: ", age)
+    return age_buckets[-1]
+
+def height_generalize(height, height_buckets):
+    for bucket in height_buckets:
+        if height >= bucket:
+            return bucket
+    # print("last bucket: ", height_buckets[-1], " height: ", height)
+    return height_buckets[-1]
+
+def weight_generalize(weight, weight_buckets):
+    for bucket in weight_buckets:
+        if weight >= bucket:
+            return bucket
+    return weight_buckets[-1]
+
+
+
+def check_k_anonymity(df, k, ages, heights, weights):
+    penalty = 0
+    for age in ages:
+        for height in heights:
+            for weight in weights:
+                count = ((df["Age"] == age) & (df["Height"] == height) & (df["Weight"] == weight)).sum()
+                penalty += count ** 2
+                if count < k and count > 0:
+                    # print("QID (", age, height, weight, ") appeared", count, "times")
+                    return (False, -1)
+    return (True, penalty)
 
 
 
@@ -107,46 +148,8 @@ def get_weight_buckets(buckets_no):
         weight_buckets = weight_buckets_3
     return weight_buckets
 
-def age_generalize(age, age_buckets):
-    # can't be generalized further
-    # if age == 0:
-    #     return 0
-    for bucket in age_buckets:
-        if age >= bucket:
-            return bucket
-    # print("last bucket: ", age_buckets[-1], " age: ", age)
-    return age_buckets[-1]
-
-def height_generalize(height, height_buckets):
-    for bucket in height_buckets:
-        if height >= bucket:
-            return bucket
-    # return 0.0
-    # print("last bucket: ", height_buckets[-1], " height: ", height)
-    return height_buckets[-1]
-
-def weight_generalize(weight, weight_buckets):
-    for bucket in weight_buckets:
-        if weight >= bucket:
-            return bucket
-    # return 0
-    return weight_buckets[-1]
-
-def check_k_anonymity(df, k, ages, heights, weights):
-    penalty = 0
-    for age in ages:
-        for height in heights:
-            for weight in weights:
-                count = ((df["Age"] == age) & (df["Height"] == height) & (df["Weight"] == weight)).sum()
-                penalty += count ** 2
-                if count < k and count > 0:
-                    print("QID (", age, height, weight, ") appeared", count, "times")
-                    return (False, -1)
-    return (True, penalty)
-
-
 penalty_dict = {}
-original = df
+original = df.copy(deep=True)
 def anonymize(df, k, pass_vector):
     if (pass_vector[0], pass_vector[1], pass_vector[2]) in penalty_dict:    #already tried
         return
@@ -173,21 +176,22 @@ def anonymize(df, k, pass_vector):
     penalty_dict[(pass_vector[0], pass_vector[1], pass_vector[2])] = check_result[1]         # negative penalty means failed
 
     #return on max depth
-    if sum(pass_vector) == 3:
-        return
+    # if sum(pass_vector) == 3:
+    #     return
     
     for i in range(len(pass_vector)):
         copy = pass_vector.copy()
-        copy[i] = copy[i] + 1
-        df = original
-        anonymize(df, k, copy)
+        if (copy[i] + 1) <= 3:      # max depth
+            copy[i] = copy[i] + 1
+            df = original.copy(deep=True)
+            anonymize(df, k, copy)
+
 
 def main():
-
     k = int(input("Enter the value of k for k-anonymity: "))
 
     # perform search for the best generalization node
-    anonymize(df, 45, [0,0,0])      # 2, 15, 45
+    anonymize(df, k, [0,0,0])      # 2, 15, 45
 
     # get the node with lowest penalty
     positive_items = {k: v for k, v in penalty_dict.items() if v > 0}
@@ -197,15 +201,22 @@ def main():
     age_buckets = get_age_buckets(min_key[0])
     height_buckets = get_height_buckets(min_key[1])
     weight_buckets = get_weight_buckets(min_key[2])
+    
+    original["Age"] = original["Age"].apply(age_generalize, args=(age_buckets,))
+    original["Height"] = original["Height"].apply(height_generalize, args=(height_buckets,))
+    original["Weight"] = original["Weight"].apply(weight_generalize, args=(weight_buckets,))
 
-    df["Age"] = df["Age"].apply(age_generalize, args=(age_buckets,))
-    df["Height"] = df["Height"].apply(height_generalize, args=(height_buckets,))
-    df["Weight"] = df["Weight"].apply(weight_generalize, args=(weight_buckets,))
+    # final check
+    group_counts = original.groupby(['Age', 'Height', 'Weight']).size()    
+    violating_groups = group_counts[group_counts < k]
+    if len(violating_groups) > 0:
+        print(f"Dataframe does NOT satisfy {k}-anonymity.")
+        print("Violating QID combinations and their counts:")
+        print(violating_groups)
+    else:
+        print(f"Dataframe satisfies {k}-anonymity.")
 
-    print(df.head(20))
-
-    df.to_csv("output.csv", index=False)
-
+    original.to_csv("output.csv", index=False)
 
 main()
 
